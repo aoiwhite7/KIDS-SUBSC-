d1a7aec345c6d6db66a5ef0e014cfee2bf1bdc62
aoiwhite7 <afurugoori@dl-techx-xedge.net>
2024-09-09 11:44:24 +0900

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,22 +1,87 @@
-new Vue({
-  el: '#app',
-  vuetify: new Vuetify(),
-  data() {
-      return {
-          tab: 0, // 初期タブ
-          dataList: [], // 会員登録情報を格納するリスト
-        user_id: sessionStorage.getItem('user_id'), // ログインセッションからユーザーIDを取得
-      };
-  },
-  methods: {
-      
-      addData() {
-        // 商品の検索画面に遷移
-        window.location.href = '/index1.html';
-      },
-  },
-  mounted() {
-      // マウント時にデータを取得
-      this.fetchData();
-  },
-});
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App開発２</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/@mdi/font@6.x/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900">
+    <link rel="stylesheet" href="./style2.css">
+</head>
+<body>
+    <div id="app">
+        <v-app>
+            <v-app-bar app class="custom-font">
+                <v-spacer></v-spacer>
+                <v-toolbar-title class="centered-title">Kids Sabsc</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-btn @click="addData" dark color="#3cb371" rounded class="custom-font">
+                    Home
+                    <v-icon start>mdi-home</v-icon>
+                </v-btn>
+            </v-app-bar>
+
+            <v-main>
+                <v-container>
+                    <!-- タブ -->
+                    <v-tabs
+                        v-model="tab"
+                        background-color="white"
+                        color="blue"
+                        align-tabs="center"
+                        class="custom-tabs"
+                    >
+                        <v-tab>会員登録情報</v-tab>
+                        <v-tab>注文履歴</v-tab>
+                    </v-tabs>
+
+                    <!-- タブのコンテンツ -->
+                    <v-tabs-items v-model="tab">
+                        <!-- 会員登録情報タブ -->
+                        <v-tab-item>
+                            <v-card flat>
+                                <v-card-text class="centered-content">
+                                    <v-row 
+                                        class="mb-3 mt-3"
+                                        justify="center"
+                                    >
+                                        <v-col cols="12">
+                                            <div>ID: {{ userData.user_id }}</div>
+                                            <div>名前: {{ userData.user_name }}</div>
+                                            <div>パスワード: {{ userData.user_pass }}</div>
+                                            <div>郵便番号: {{ userData.user_postcode }}</div>
+                                            <div>住所: {{ userData.user_adress }}</div>
+                                            <div>電話番号: {{ userData.user_telenum }}</div>
+                                        </v-col>
+                                    </v-row>
+                                </v-card-text>
+                            </v-card>
+                        </v-tab-item>
+
+                        <!-- 注文履歴タブ -->
+                        <v-tab-item>
+                            <v-card flat>
+                                <v-card-text class="centered-content">
+                                    <p>注文履歴の詳細がここに表示されます。</p>
+                                </v-card-text>
+                            </v-card>
+                        </v-tab-item>
+                    </v-tabs-items>
+                </v-container>
+            </v-main>
+
+            <v-footer app padless class="custom-font">
+                <v-col class="text-center" cols="12">
+                    © 2024 TeamA
+                </v-col>
+            </v-footer>
+        </v-app>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.11/vue.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.7.2/axios.min.js"></script>
+    <script src="./script2.js"></script>
+</body>
+</html>
